4e934b06529f6965d7f57278ee1067b582f18c11
Maruthapandi142434 <134052444+Maruthapandi142434@users.noreply.github.com>
2026-04-27 13:05:42 +0530

    gtm added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Outfit } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -24,6 +25,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KQTWE54R4G"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-KQTWE54R4G');
+          `}
+        </Script>
+      </head>
       <body
         className={`${inter.variable} ${outfit.variable} antialiased`}
       >
